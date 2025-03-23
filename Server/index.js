00db2301f93208846fe87c171d176a9d96507fe9
fethulmubin <fetihul@gmail.com>
2025-03-23 17:29:20 +0300

    decode and jsonwebtoken

--- Server/index.js
+++ Server/index.js
@@ -18,6 +18,7 @@ const PORT = process.env.PORT || 5555;
 const CONNECTION_URL = "mongodb+srv://fetihul:AN8BqTMIYsH0V9oa@cluster0.80jdc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Connect to MongoDB
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+// mongodb+srv://fetihul:<db_password>@cluster0.80jdc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+mongoose.connect(CONNECTION_URL)
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
   .catch((error) => console.error(error.message));
