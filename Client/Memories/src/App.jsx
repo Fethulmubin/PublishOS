8feaaff4b71f0b2455288872967026c1b8ec888f
fethulmubin <fetihul@gmail.com>
2025-03-25 18:32:55 +0300

    react router added

--- Client/Memories/src/App.jsx
+++ Client/Memories/src/App.jsx
@@ -1,50 +1,24 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import { Bar } from './styles'
-import { Container, AppBar, Typography, Grow, Grid } from '@mui/material'
-import Posts from './components/Posts/Posts'
-import Form from './components/Form/Form'
+import { Container } from '@mui/material'
 import { styled } from "@mui/system";
 import { Styles } from './styles'
-import memory from './assets/memory.png'
-import {BrowserRouter, Swich, Route} from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { getPost } from './actions/posts'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Navbar from './components/NavBar/Navbar'
+import Home from './components/Home/Home'
 
 function App() {
 // const classes = styled();
-const [currentId, setCurrentId] = useState(null)
-const StyledBar =  styled(AppBar)(()=> (Styles.appBar))
-const StyledTypography =  styled(Typography)(()=> (Styles.heading))
-
-const dispatch = useDispatch();
-
-useEffect(()=>{
-dispatch(getPost())
-},[dispatch])
   return (
-    
-      <Container maxWidth='lg'>
-        {/* <StyledBar  position='static' color='inherit'>
-          <StyledTypography  variant='h2' align='center'>Memories</StyledTypography>
-          <img className={Styles.image} src={memory} alt="memories" height='100' />
-        </StyledBar> */}
+    <BrowserRouter>
+      <Container maxWidth='lg'> 
         <Navbar/>
-        <Grow in>
-          <Container>
-            <Grid container justifyContent='space-between' alignItems='stretch' spacing={3}>
-              <Grid item xs={12} sm={7}>
-                <Posts setCurrentId={setCurrentId}/>
-              </Grid>
-              <Grid item xs={12} sm={4}>
-                <Form currentId = {currentId} setCurrentId={setCurrentId} getPost={getPost}/>
-              </Grid>
-            </Grid>
-          </Container>
-        </Grow>
+        <Switch>
+        <Route path='/' exact component ={Home} />
+        <Route path='/auth' exact component ={Home} />
+        </Switch>
+   
       </Container>
+      </BrowserRouter>
 
   )
 }
--- Client/Memories/src/components/Home/Home.jsx
+++ Client/Memories/src/components/Home/Home.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import {Styles} from './Styles'
+import React, { useState, useEffect } from 'react'
+import { Styles } from './Styles'
+import { useDispatch } from 'react-redux'
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material'
+import Posts from '../Posts/Posts'
+import Form from '../Form/Form'
+import { getPost } from '../../actions/posts'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    const [currentId, setCurrentId] = useState(null)
+    const StyledBar = styled(AppBar)(() => (Styles.appBar))
+    const StyledTypography = styled(Typography)(() => (Styles.heading))
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPost())
+    }, [dispatch])
+    return (
+        <Grow in>
+            <Container>
+                <Grid container justifyContent='space-between' alignItems='stretch' spacing={3}>
+                    <Grid item xs={12} sm={7}>
+                        <Posts setCurrentId={setCurrentId} />
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Form currentId={currentId} setCurrentId={setCurrentId} getPost={getPost} />
+                    </Grid>
+                </Grid>
+            </Container>
+        </Grow>
+    )
 }
 
 export default Home
