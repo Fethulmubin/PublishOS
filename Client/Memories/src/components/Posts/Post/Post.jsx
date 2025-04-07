d7cb2f84f9e29e00a5388c855a07d9deb31853cd
fethulmubin <fetihul@gmail.com>
2025-04-07 09:34:03 +0300

    like fuctionality updated by rendering white for unliked and blue with liked

--- Client/Memories/src/components/Posts/Post/Post.jsx
+++ Client/Memories/src/components/Posts/Post/Post.jsx
@@ -4,32 +4,36 @@ import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 import CommentIcon from '@mui/icons-material/Comment';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
-import {styled} from '@mui/system'
+import { styled } from '@mui/system'
 import { Styles } from './styles'
 import { useState, useEffect } from 'react';
 import moment from 'moment'
 import { useDispatch } from 'react-redux';
-import  {deletePost, likePost}  from '../../../actions/posts';
+import { deletePost, likePost } from '../../../actions/posts';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import CommentBar from '../../CommentBar/CommentBar';
 import { useParams, useSearchParams } from 'react-router-dom';
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import { useSelector } from 'react-redux';
 
 
 
-function Post({post, setCurrentId, currentId}) {
+function Post({ post, setCurrentId, currentId }) {
 
   const [Render, setRender] = useState(false)
   const [comment, setComment] = useState(false)
+  const user = useSelector((state) => state?.auth.authData);
   const [searchParams, setSearchParams] = useSearchParams();
   // console.log(searchParams.get('id'))
   // const {id} = useParams()
+  // console.log(user.result._id)
 
   const dispatch = useDispatch();
-  const StyledCard = styled(Card)(()=> Styles.card)
-  const StyledTypography= styled(Typography)(()=> Styles.title)
-  const StyledCardActions= styled(CardActions)(()=> Styles.cardActions)
-  const StyledCardMedia = styled(CardMedia)(()=> Styles.media)
+  const StyledCard = styled(Card)(() => Styles.card)
+  const StyledTypography = styled(Typography)(() => Styles.title)
+  const StyledCardActions = styled(CardActions)(() => Styles.cardActions)
+  const StyledCardMedia = styled(CardMedia)(() => Styles.media)
 
   useEffect(() => {
     if (Render) {
@@ -38,66 +42,67 @@ function Post({post, setCurrentId, currentId}) {
     }
   }, [Render]);
 
-  const handleSubmit = ()=>{
+  const handleLike = () => {
     dispatch(likePost(post._id))
-    setRender(!Render)
+    // setRender(!Render)
   }
   return (
     <>
-   <StyledCard style={{ marginBottom: '10px' }}>
-      <StyledCardMedia image= {post.selectedFile}
-      title={post.title}/>
+      <StyledCard style={{ marginBottom: '10px' }}>
+        <StyledCardMedia image={post.selectedFile}
+          title={post.title} />
         <div className={Styles.overlay}>
           <StyledTypography variant='h6'>
-              {`Posted by ${post.creator}`}
+            {`Posted by ${post.creator}`}
           </StyledTypography>
           <StyledTypography variant='body2'>
-              {post.title}
+            {post.title}
           </StyledTypography>
           <StyledTypography variant='body2'>
-              {moment(post.createdAt).fromNow()}
+            {moment(post.createdAt).fromNow()}
           </StyledTypography>
         </div>
         <div className={Styles.details}>
-           <StyledTypography variant='body2' color='textSecondary'>
-              {post.tags.map(tag=>`#${tag} `)}
+          <StyledTypography variant='body2' color='textSecondary'>
+            {post.tags.map(tag => `#${tag} `)}
           </StyledTypography>
         </div>
         <CardContent>
-           <StyledTypography variant='h5' gutterBottom>
-                  {post.message}
-           </StyledTypography>
+          <StyledTypography variant='h5' gutterBottom>
+            {post.message}
+          </StyledTypography>
         </CardContent>
         <StyledCardActions>
-            <Button size='small' style={{color : '#74a1e8'}} onClick={()=> {
-              handleSubmit()
-            }}>
-              <ThumbUpAltIcon style={{color : '#74a1e8'}} fontSize= 'small'/>
-              {post.likeCount}
-            </Button>
-            <Button size='small' style={{color : '#e36c27'}} onClick={()=> 
-              {dispatch(deletePost(post._id))
-              }}>
-              <DeleteIcon style={{color : '#e36c27'}} fontSize= 'small'/>
-            </Button>
-            {/* {deletePosts && <ToastContainer />} */}
-            <div className={Styles.overlay2} style={{display: 'flex', gap: '5px'}}>
-          <Button style={{color : 'blue'}} size = 'small' onClick={()=> 
-            {setCurrentId(post._id)
-            }}>
-              <EditIcon style={{color : '#74a1e8', fontSize: '20px'}} />
+          <Button size='small' style={{ color: '#74a1e8' }} onClick={handleLike}>
+            {user && post.likes.includes(user.result._id)
+              ? <ThumbUpAltIcon style={{ color: '#74a1e8' }} fontSize='small' />
+              : <ThumbUpOffAltIcon style={{ color: '#74a1e8' }} fontSize='small' />
+            }
+            {post.likes.length}
           </Button>
-          <Button style={{color : 'blue'}} size = 'small' onClick={()=> 
-            {searchParams.get('id') ? setSearchParams({}) : setSearchParams({id: post._id})
-            }}>
-              <CommentIcon style={{color : '#74a1e8', fontSize: '20px'}} />
+          <Button size='small' style={{ color: '#e36c27' }} onClick={() => {
+            dispatch(deletePost(post._id))
+          }}>
+            <DeleteIcon style={{ color: '#e36c27' }} fontSize='small' />
           </Button>
-           </div>
+          {/* {deletePosts && <ToastContainer />} */}
+          <div className={Styles.overlay2} style={{ display: 'flex', gap: '5px' }}>
+            <Button style={{ color: 'blue' }} size='small' onClick={() => {
+              setCurrentId(post._id)
+            }}>
+              <EditIcon style={{ color: '#74a1e8', fontSize: '20px' }} />
+            </Button>
+            <Button style={{ color: 'blue' }} size='small' onClick={() => {
+              searchParams.get('id') ? setSearchParams({}) : setSearchParams({ id: post._id })
+            }}>
+              <CommentIcon style={{ color: '#74a1e8', fontSize: '20px' }} />
+            </Button>
+          </div>
         </StyledCardActions>
-   </StyledCard>
-   
-  
-   </>
+      </StyledCard>
+
+
+    </>
   )
 }
 
